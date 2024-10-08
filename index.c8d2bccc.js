881f7a5f5dbc3c165a407cb3d54b9fe0030e17bf
Ilona Gogol <ilonka.gogol@gmail.com>
2024-10-08 21:39:28 +0300

    make build

---
+++ index.c8d2bccc.js
@@ -0,0 +1,2 @@
+function e(e){return e.replace(/([A-Z])/g," $1").replace(/^./,function(e){return e.toUpperCase()})}document.querySelectorAll(".field-text").forEach(function(t){var r=t.getAttribute("id"),n=t.getAttribute("name"),l=document.createElement("label");l.htmlFor=r,l.textContent=e(n),l.className="field-label",t.insertAdjacentElement("beforebegin",l),t.setAttribute("placeholder",e(n))});
+//# sourceMappingURL=index.c8d2bccc.js.map
